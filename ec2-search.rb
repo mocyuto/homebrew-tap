eb0ba4088d3f911398df8cb28e601c3766c319bd
Yuto Suzuki <yuutoo.advance@gmail.com>
2020-09-07 19:55:54 +0900

    first commit

---
+++ ec2-search.rb
@@ -0,0 +1,17 @@
+class Ec2Search < Formula
+  desc "search ec2 instance easily"
+  homepage ""
+  url "https://github.com/mocyuto/ec2-search/releases/download/v0.1.0/ec2-search-x86_64-apple-darwin.tar.gz"
+  sha256 "6f369f92eb856fddb9322dbf44dd2aa675f2a733328c7f5c3e3fa04da5b7c6fd"
+  license "Apache-2.0"
+
+  # depends_on "cmake" => :build
+
+  def install
+    bin.install "ec2-search"
+  end
+
+  test do
+    system "true"
+  end
+end
