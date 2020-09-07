0018d704a338c43b313037c9bc54af5a9126344d
Yuto Suzuki <yuutoo.advance@gmail.com>
2020-09-07 20:21:04 +0900

    add version

--- ec2-search.rb
+++ ec2-search.rb
@@ -4,8 +4,7 @@ class Ec2Search < Formula
   url "https://github.com/mocyuto/ec2-search/releases/download/v0.1.0/ec2-search-x86_64-apple-darwin.tar.gz"
   sha256 "6f369f92eb856fddb9322dbf44dd2aa675f2a733328c7f5c3e3fa04da5b7c6fd"
   license "Apache-2.0"
-
-  # depends_on "cmake" => :build
+  version "0.1.0"
 
   def install
     bin.install "ec2-search"
