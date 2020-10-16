ab365d36c0424d233e46b95f127dfdf14b93a8a1
Yuto Suzuki <yuutoo.advance@gmail.com>
2020-10-16 19:52:58 +0900

    fix audit

--- ec2-search.rb
+++ ec2-search.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 class Ec2Search < Formula
-  desc "search ec2 instance easily"
-  homepage ""
+  desc "Search ec2 instance easily"
+  homepage "https://github.com/mocyuto/ec2-search"
   url "https://github.com/mocyuto/ec2-search/releases/download/v0.2.0/ec2-search-x86_64-apple-darwin.tar.gz"
+  version "0.2.0"
   sha256 "8b8379a15fc144cefd8e27aadc611abc85bccd2b34a6f11795815e4b4620536e"
   license "Apache-2.0"
-  version "0.1.0"
 
   def install
     bin.install "ec2-search"
