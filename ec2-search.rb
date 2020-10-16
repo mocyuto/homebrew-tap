41312067f9a996f71b6cde4aa63737fe06150c49
Yuto Suzuki <yuutoo.advance@gmail.com>
2020-10-16 19:41:50 +0900

    v0.2.0

--- ec2-search.rb
+++ ec2-search.rb
@@ -1,8 +1,8 @@
 class Ec2Search < Formula
   desc "search ec2 instance easily"
   homepage ""
-  url "https://github.com/mocyuto/ec2-search/releases/download/v0.1.0/ec2-search-x86_64-apple-darwin.tar.gz"
-  sha256 "6f369f92eb856fddb9322dbf44dd2aa675f2a733328c7f5c3e3fa04da5b7c6fd"
+  url "https://github.com/mocyuto/ec2-search/releases/download/v0.2.0/ec2-search-x86_64-apple-darwin.tar.gz"
+  sha256 "8b8379a15fc144cefd8e27aadc611abc85bccd2b34a6f11795815e4b4620536e"
   license "Apache-2.0"
   version "0.1.0"
 
