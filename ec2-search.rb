54c7da0a7ff66d1bcfe41fc284d09f5c68575b89
Yuto Suzuki <yuutoo.advance@gmail.com>
2020-11-06 23:08:14 +0900

    release v0.3.2

--- ec2-search.rb
+++ ec2-search.rb
@@ -3,17 +3,17 @@
 class Ec2Search < Formula
   desc "Search ec2 instance easily"
   homepage "https://github.com/mocyuto/ec2-search"
-  version "v0.3.1"
+  version "v0.3.2"
   license "Apache-2.0"
 
   bottle :unneeded
 
   if OS.mac?
     url "https://github.com/mocyuto/ec2-search/releases/download/#{version}/ec2-search-macos.tar.gz"
-    sha256 "47c75001c5d01561ecedcd543117f813dc58892e8d252676de936a199b4d659c"
+    sha256 "b9fac593a9c078d4d60f44f0dd6508452d2b5775226906e257434b74b9c8d265"
   elsif OS.linux?
     url "https://github.com/mocyuto/ec2-search/releases/download/#{version}/ec2-search-linux.tar.gz"
-    sha256 "f90c297f62b22f893c985a9f90eb01cf728f866c92784918f36c8a2178ad001b"
+    sha256 "ae2d3c2c6f56781f3c81b333e0e2574ed3d3cccd75c4580281127c0c6dfc07b6"
   end
 
   def install
