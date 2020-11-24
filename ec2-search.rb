a5a1896176f012a8e0ca502ada8f7e4442c5574b
Yuto Suzuki <yuutoo.advance@gmail.com>
2020-11-24 19:32:05 +0900

    v0.7.0

--- ec2-search.rb
+++ ec2-search.rb
@@ -3,17 +3,17 @@
 class Ec2Search < Formula
   desc "Search AWS EC2 easily"
   homepage "https://github.com/mocyuto/ec2-search"
-  version "v0.6.0"
+  version "v0.7.0"
   license "Apache-2.0"
 
   bottle :unneeded
 
   if OS.mac?
     url "https://github.com/mocyuto/ec2-search/releases/download/#{version}/ec2-search-macos.tar.gz"
-    sha256 "14d0258f70d31cc1ce14daa0942861af74871149bf5e46470c21e69dd4ac72ff"
+    sha256 "fa36af5e2f00483348d5323514be7f54093b916cec25e16a559c7dbebae20c10"
   elsif OS.linux?
     url "https://github.com/mocyuto/ec2-search/releases/download/#{version}/ec2-search-linux.tar.gz"
-    sha256 "712b6b5148b7dbd945995b912829289ecd43483a9a1dd62f728aca0ab413009f"
+    sha256 "ecbe9a1addd6779a5809db30b85ae2e93839ae50f4ff2718196fc88c1486820b"
   end
 
   def install
