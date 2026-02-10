d3e34a03dabc6307c8622d48d06b168ff4af9c11
Yuto Suzuki <yuutoo.advance@gmail.com>
2026-02-10 14:46:45 +0900

    git-wt v0.2.1

--- git-wt.rb
+++ git-wt.rb
@@ -3,24 +3,24 @@
 class GitWt < Formula
   desc 'Git Worktree helper CLI'
   homepage 'https://github.com/mocyuto/git-wt'
-  version 'v0.2.0'
+  version 'v0.2.1'
   license 'MIT'
 
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/mocyuto/git-wt/releases/download/#{version}/git-wt_darwin_arm64.tar.gz"
-      sha256 'ff40985f77e414279e150f0df598216d83014c1936c1e23d311db74967f97dd8'
+      sha256 '3ef5b74bad2ab3dc3ecd7e50e8593aeeabb5fb479ff4a1dc5f9b13569c0d269d'
     else
       url "https://github.com/mocyuto/git-wt/releases/download/#{version}/git-wt_darwin_amd64.tar.gz"
-      sha256 '335e3c4a9b4b8a4f339436da493bae426fcd9db637c01714b06fe0c1a174eba6'
+      sha256 'a79ebb9ce4cd40c46caf72204d09ec48a9764d25c8d1f1bdc969690712df36ee'
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
       url "https://github.com/mocyuto/git-wt/releases/download/#{version}/git-wt_linux_arm64.tar.gz"
-      sha256 '32a3f7f0e240d80f1fcf7041fac7629e44d828108edf780ca51d158d7b6f6efe'
+      sha256 'bd455cdb69eca4488a23280874a84f25661b5adc721f50eb7e858a095605abe4'
     else
       url "https://github.com/mocyuto/git-wt/releases/download/#{version}/git-wt_linux_amd64.tar.gz"
-      sha256 '212a8d3f125b842bbe396d027e406027e8130c832a6d482c0e524b58f0734db0'
+      sha256 'f530b3209cc3d80247ed2a1faa052bc623e3c732933be50637b72a0284bb540e'
     end
   end
 
