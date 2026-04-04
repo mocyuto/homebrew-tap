6c748ee09cc3b18255eaf8ae13714a7c4fb3af71
Yuto Suzuki <yuutoo.advance@gmail.com>
2026-04-04 18:31:51 +0900

    zgt v0.13.0

--- Formula/zgt.rb
+++ Formula/zgt.rb
@@ -3,24 +3,24 @@
 class Zgt < Formula
   desc 'Git Worktree helper CLI'
   homepage 'https://github.com/mocyuto/zgt'
-  version 'v0.12.1'
+  version 'v0.13.0'
   license 'MIT'
 
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/mocyuto/zgt/releases/download/#{version}/zgt_darwin_arm64.tar.gz"
-      sha256 'd823e4a0c193d887d1ba70b35df69a0f1bd55a1dd015bd3c5addbbc69056e390'
+      sha256 '86ed62343c0abc1461348e57624e0b5e326df6856d26961445713f63670748e1'
     else
       url "https://github.com/mocyuto/zgt/releases/download/#{version}/zgt_darwin_amd64.tar.gz"
-      sha256 'd62e79c77055817ec54f690676bd5495a9683f7d73876147c2d37a006844accd'
+      sha256 '79895c09113c10733610865e1acd560da5d0c97d6682869c885d0f1ae9abfdc4'
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
       url "https://github.com/mocyuto/zgt/releases/download/#{version}/zgt_linux_arm64.tar.gz"
-      sha256 'bf2c073c6df12d38c860b7f117510bbbc9fa707231d0ab7df462b4cb0ae8136e'
+      sha256 '24b3c2e0eaa3bdc59ffe29c1b7a15053ad99675fb244a7a8670a3514485079a7'
     else
       url "https://github.com/mocyuto/zgt/releases/download/#{version}/zgt_linux_amd64.tar.gz"
-      sha256 'fe8b27e1ea01c0278df28f949cd8161ee30bd32411bd1a4399986b269db93c35'
+      sha256 'f478d182d8c32e4b1a5c5fcefc81039e6153ae8ec8d8c1010204bd105ac99a8c'
     end
   end
 
--- Formula/zgt@0.13.0.rb
+++ Formula/zgt@0.13.0.rb
@@ -1,26 +1,26 @@
 # frozen_string_literal: true
 
-class ZgtAT081 < Formula
+class ZgtAT0130 < Formula
   desc 'Git Worktree helper CLI'
   homepage 'https://github.com/mocyuto/zgt'
-  version 'v0.8.1'
+  version 'v0.13.0'
   license 'MIT'
 
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/mocyuto/zgt/releases/download/#{version}/zgt_darwin_arm64.tar.gz"
-      sha256 'a48b41980871b730f8e9a5bf77efecbff42e4115a22ae3ad879d7e3527aa1336'
+      sha256 '86ed62343c0abc1461348e57624e0b5e326df6856d26961445713f63670748e1'
     else
       url "https://github.com/mocyuto/zgt/releases/download/#{version}/zgt_darwin_amd64.tar.gz"
-      sha256 'c7003a883aca88beb7b33da16c7056518337636c262682b68dd31f39747bdf5c'
+      sha256 '79895c09113c10733610865e1acd560da5d0c97d6682869c885d0f1ae9abfdc4'
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
       url "https://github.com/mocyuto/zgt/releases/download/#{version}/zgt_linux_arm64.tar.gz"
-      sha256 '266158d8d798f2f4be4f850cd31a92aaf44a4ffe35111d0f688f541cc67695bb'
+      sha256 '24b3c2e0eaa3bdc59ffe29c1b7a15053ad99675fb244a7a8670a3514485079a7'
     else
       url "https://github.com/mocyuto/zgt/releases/download/#{version}/zgt_linux_amd64.tar.gz"
-      sha256 '70cd8ce38fe26bc40c306e3527c821ba86d95a19ad43875ac49178c7e2d3513a'
+      sha256 'f478d182d8c32e4b1a5c5fcefc81039e6153ae8ec8d8c1010204bd105ac99a8c'
     end
   end
 
